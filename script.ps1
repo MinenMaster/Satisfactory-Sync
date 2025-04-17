6fb1404f04af2a6aaf72091dd8883ed5ff9b3c5b
Dominik Meister <95213203+MinenMaster@users.noreply.github.com>
2025-04-17 11:48:18 +0200

    feat: add processName variable

--- script.ps1
+++ script.ps1
@@ -2,8 +2,9 @@ param([String]$game="")
 
 # Define variables
 $repoPath = "C:\Path\To\Your\Repo"  # Path where the GitHub repository is cloned
-$savePath = "C:\Users\<YourUsername>\AppData\Local\FactoryGame\Saved\SaveGames\<YourSteamID>"
+$savePath = "C:\Users\<YourUsername>\AppData\Local\FactoryGame\Saved\SaveGames\<YourSteamID>" # Path to the Satisfactory save files
 $saveFile = "YourSaveFileName.sav"
+$processName = "FactoryGameSteam"  # Process name for Satisfactory
 
 # Pull the latest save file
 Set-Location $repoPath
@@ -14,10 +15,10 @@ Copy-Item "$repoPath\$saveFile" "$savePath\$saveFile" -Force
 Start-Process $game # Satisfactory Steam ID
 
 # Wait for the game to exit
-$gameProcess = Get-Process -Name "FactoryGameSteam" -ErrorAction SilentlyContinue
+$gameProcess = Get-Process -Name $processName -ErrorAction SilentlyContinue
 while ($null -ne $gameProcess) {
     Start-Sleep -Seconds 5
-    $gameProcess = Get-Process -Name "FactoryGameSteam" -ErrorAction SilentlyContinue
+    $gameProcess = Get-Process -Name $processName -ErrorAction SilentlyContinue
 }
 
 # After game exit, push the save file back to GitHub
